4a647a89910ed8ad2c0182d2b84a03da831b8f87
gluay88 <p.wantagaew@gmail.comm>
2022-12-27 14:51:03 -0700

    added currentYear for the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 
+var currentYear = new Date().getFullYear();
+
 class Footer extends Component {
   render() {
     return (
       <footer className="footer-main">
         <h2>
-          &copy; copyright 2022 -{" "}
-          <a href="https://gluay88.github.io/portfolio-website">Glu@y</a>
+          &copy; copyright {currentYear} -
+          <a href="https://gluay88.github.io/portfolio-website"> Glu@y</a>
         </h2>
       </footer>
     );
